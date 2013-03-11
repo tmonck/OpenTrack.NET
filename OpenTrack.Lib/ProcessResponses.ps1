fa3c104f8cbf45193375b46df129f99357ee025f
Matt Wagner <mattgwagner@gmail.com>
2013-03-11 13:56:11 -0400

    Moved the generated classes into OpenTrack.Responses namespace.

--- OpenTrack.Lib/ProcessResponses.ps1
+++ OpenTrack.Lib/ProcessResponses.ps1
@@ -5,7 +5,7 @@ Set-Alias xsd $Root\xsd.exe
 pushd Responses
 
 ls *.xml | foreach-object { xsd $_ }
-ls *.xsd | foreach-object { xsd $_  /classes}
+ls *.xsd | foreach-object { xsd $_  /classes /language:CS /namespace:OpenTrack.Responses}
 
 foreach($file in (Get-ChildItem *.cs))
 {
