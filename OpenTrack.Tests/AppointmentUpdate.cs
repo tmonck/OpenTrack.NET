a6322d548fd66f34dc90e9472ab9cc56d69d302b
Matt Wagner <mattgwagner@gmail.com>
2013-04-25 10:57:40 -0400

    Added quick test for updating appointments.
    Doesn't work at the moment.

---
+++ OpenTrack.Tests/AppointmentUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace OpenTrack.Tests
+{
+    public class AppointmentUpdate
+    {
+        // [Fact]
+        public void Test_Update_Appointment()
+        {
+            var api = Credentials.GetAPI();
+
+            api.UpdateAppointment(new Requests.AppointmentUpdateRequest(Credentials.EnterpriseCode, Credentials.DealerCode, Credentials.ServerName)
+            {
+                AppointmentNumber = "338",
+                AppointmentDateTime = new DateTime(2013, 5, 5, 12, 0, 0),
+                ServiceWriterID = "DT",
+                CompanyNumber = "ZE7"
+            });
+        }
+    }
+}
