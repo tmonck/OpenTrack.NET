89d0682696739b7e78a627bb18db6bc30f311805
Matt Wagner <mattgwagner@gmail.com>
2013-08-14 16:37:48 -0400

    Removing errant 'NewDataSet' from xsd.exe

--- OpenTrack.Lib/Responses/AddRepairOrderLinesResponse.cs
+++ OpenTrack.Lib/Responses/AddRepairOrderLinesResponse.cs
@@ -72,27 +72,4 @@ namespace OpenTrack.Responses {
             }
         }
     }
-    
-    /// <remarks/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.0.30319.17929")]
-    [System.SerializableAttribute()]
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType=true)]
-    [System.Xml.Serialization.XmlRootAttribute(Namespace="", IsNullable=false)]
-    public partial class NewDataSet {
-        
-        private AddRepairOrderLinesResponse[] itemsField;
-        
-        /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute("AddRepairOrderLinesResponse")]
-        public AddRepairOrderLinesResponse[] Items {
-            get {
-                return this.itemsField;
-            }
-            set {
-                this.itemsField = value;
-            }
-        }
-    }
 }
