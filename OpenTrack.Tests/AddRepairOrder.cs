112b439f965e417eae92e27c371147bc245929e1
Matt Wagner <mattgwagner@gmail.com>
2013-08-14 16:49:11 -0400

    Initial work on the RO line adding "test"

--- OpenTrack.Tests/AddRepairOrder.cs
+++ OpenTrack.Tests/AddRepairOrder.cs
@@ -7,32 +7,56 @@ namespace OpenTrack.Tests
 {
     public class AddRepairOrder
     {
+        private readonly Boolean Run = false;
+
         [Fact]
-        public void Test_Add_Repair_Order()
+        public void Add_Repair_Order_Line()
         {
+            if (!Run) return;
+
             var api = Credentials.GetAPI();
 
-            api.AddRepairOrder(new Requests.AddRepairOrderRequest(Credentials.EnterpriseCode, Credentials.DealerNumber)
+            // Grab RO's from the past two hours
+            var ROs = api.FindOpenRepairOrders(new OpenRepairOrderLookup(Credentials.EnterpriseCode, Credentials.DealerNumber)
             {
-                RO = new Requests.RepairOrder()
-                {
-                    CustomerNumber = "",
-                    OdometerIn = "",
-                    PromisedDateTime = "",
-                    ServiceWriterID = "",
-                    TagNumber = "",
-                    VIN = "",
-                    LineItems = new[]
-                    {
-                        new LineItem()
-                        {
+                CreatedDateTimeStart = DateTime.UtcNow.AddHours(-2),
+                CreatedDateTimeEnd = DateTime.UtcNow
+            });
 
-                        }
-                    }.ToList()
+            // We're just going to use the first one in the list for this
+            var RO = ROs.First();
+
+            // Add a line
+            api.AddRepairOrderLines(new AddRepairOrderLinesRequest(Credentials.EnterpriseCode, Credentials.DealerNumber)
+            {
+                RepairOrderNumber = RO.RepairOrderNumber,
+                ServiceWriterID = "485", // One selected randomly, not test-friendly!
+                VIN = RO.VIN,
+                CustomerNumber = RO.CustomerNumber,
+                LineItems = new[]
+                {
+                    new LineItem()
+                    {
+                        LaborOpCode = "*", // See ServiceLaborOpcodesTable, * for undefined
+                        ServiceLineNumber = "1", // The line # this detail refers to, grouped together by this
+                        LineType = "A", // A = Labor Op Code Line Desc
+                        TransCode = "CP", // CP IS WS
+                        Comments = "This is a test line!", // Optional
+                        LineStatus = "", // Optional I = Unassigned C = Complete
+                        ServiceType = "MR"
+                    }
                 }
             });
 
-            // Get the open repair orders and check that this one was added.
+            // Pull the RO again and verify additional line
+
+            var RO_Check = api.FindOpenRepairOrders(new OpenRepairOrderLookup(Credentials.EnterpriseCode, Credentials.DealerNumber)
+            {
+                RepairOrderNumber = RO.RepairOrderNumber
+            })
+            .First();
+
+            // Assert.True(RO_Check.Details.Any(line => line.LaborOpCode == OpCode));
 
         }
     }
