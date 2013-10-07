2f7856c9db398301f69cc9f788a4e2a0f8290735
Tom Monck <tom@asrpro.com>
2013-10-07 13:22:21 -0400

    Finish adding the PartTransactionsRequest
    Add PartsTransactionsRequestResponse

---
+++ OpenTrack.Tests/GetPartsTransactions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace OpenTrack.Tests
+{
+    public class GetPartsTransactions
+    {
+        [Fact]
+        public void Parts_Transactions_Per_Manufacturer()
+        {
+            var api = Credentials.GetAPI();
+
+            foreach (var manufacturer in api.GetPartManufacturers(new Requests.PartsManufacturersTableRequest(Credentials.EnterpriseCode, Credentials.DealerNumber)))
+            {
+                foreach (var part in api.GetPartsTransactions(new Requests.PartsTransactionsRequest(Credentials.EnterpriseCode, Credentials.DealerNumber) { Manufacturer = manufacturer.Manufacturer }))
+                {
+                    Assert.False(String.IsNullOrWhiteSpace(part.PartNumber));
+                }
+            }
+        }
+    }
+}
