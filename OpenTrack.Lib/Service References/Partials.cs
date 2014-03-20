81be1d9eabd77b937afe3157a0c4d874c0287e01
Brian French <brian@asrpro.com>
2014-03-20 14:06:51 -0400

    Added partial classes for the closed repair order requests.  Now if the RepairOrderNumber is less than one, it will not serialize.

---
+++ OpenTrack.Lib/Service References/Partials.cs
@@ -0,0 +1,20 @@
+﻿namespace OpenTrack.ServiceAPI
+{
+    public partial class GetClosedRepairOrdersRequest
+    {
+        public bool ShouldSerializeRepairOrderNumber()
+        {
+            // fixes issue where if RepairOrderNumber == 0 it serializes with 0 and the call returns zero results
+            return this.RepairOrderNumber > 0;
+        }
+    }
+
+    public partial class GetClosedRepairOrderDetailRequest
+    {
+        public bool ShouldSerializeRepairOrderNumber()
+        {
+            // fixes issue where if RepairOrderNumber == 0 it serializes with 0 and the call returns zero results
+            return this.RepairOrderNumber > 0;
+        }
+    }
+}
