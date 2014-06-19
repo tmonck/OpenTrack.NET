f201ede4120cdf0f41748dc0baf382edaa9bb89f
Matt Wagner <mattgwagner@gmail.com>
2014-06-19 09:27:01 -0400

    Update NuGet.exe and packaging PS

--- Build-Package.ps1
+++ Build-Package.ps1
@@ -2,4 +2,4 @@ $BaseDir = Split-Path (Resolve-Path $MyInvocation.MyCommand.Path)
 
 mkdir "$BaseDir\Artifacts" -Force
 
-& "$BaseDir\.NuGet\NuGet.exe" Pack OpenTrack.nuspec -OutputDirectory "$BaseDir\artifacts\"
+& "$BaseDir\.NuGet\NuGet.exe" Pack OpenTrack.nuspec -Properties Configuration=Release -OutputDirectory "$BaseDir\artifacts\"
